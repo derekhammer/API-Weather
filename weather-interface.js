d8f24580e81f7c4d7e6f8b4eaa5131e07696de52
Derek Hammer <derekhammer@hotmail.com>
2018-08-07 16:17:28 -0700

    Work in progress

---
+++ weather-interface.js
@@ -0,0 +1,51 @@
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+//     $.ajax({
+//       url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ef107cf55f3eccd05c60cd97a2b68ecc`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//         $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+//         $('.showWeather').text(`The Weather is ${response.name}.`);
+//       },
+//       error: function() {
+//         $('#errors').text("There was an error processing your request. Please try again.");
+//       }
+//     });
+//   });
+// });
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=process.env.API_K`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showWeather').text(`The Weather is ${response.name}.`);
+  //   }
+  //   error: function() {
+  //     $('#errors').text("There was an error processing your request. Please try again.");
+}
+
+  });
+});
